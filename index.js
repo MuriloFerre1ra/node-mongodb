f1c4290fbd3999af9d66b615c947808a3f8b3ffc
Murilex1 <murilo.ferreira32@outlook.com>
2024-08-21 21:56:41 -0300

    connected mongo and send data for db

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 import express from "express";
 import mongoose from "mongoose";
 
+import User from "./models/User.js";
+
+const app = express();
+
 app.use(express.json());
 
 const users = [];
 
-app.get('/users', (request, response) => {
-    return response.json(users);
+app.get("/users", async (request, response) => {
+  const users = await User.find();
+
+  return response.status(200).json(users);
 });
 
-app.post("/users", (request, response) => {
-    const { name, age, nickName} = request.body;
+app.post("/users", async (request, response) => {
+  const user = request.body;
 
-    user.push({ name, age, nickName});
+  const newUser = await User.create(user);
 
-    return response.json({ name, age, nickName});    
-})
+  return response.status(201).json(newUser);
+});
 
-mongoose.connect('mongodb+srv://muriloti100:senhateste@cluster0.5c3on.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-.then(() => console.log("Database connected."))
-.catch(() => console.log("Error connecting database"))
+mongoose
+  .connect(
+    "mongodb+srv://muriloti100:senhateste@cluster0.5c3on.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => console.log("Database connected."))
+  .catch(() => console.log("Error connecting database"));
 
 app.listen(3000);
